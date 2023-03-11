113cbb891c2e0745d1666df789707f5d24cf8965
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-03-12 01:27:06 +0530

    Add Send Functionality

--- app/src/main/java/com/example/easychatgpt/MainActivity.java
+++ app/src/main/java/com/example/easychatgpt/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.easychatgpt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    TextView welcomeTextView;
+    EditText messageEditText;
+    ImageButton sendButton;
+    List<Message> messageList;
+    MessageAdapter messageAdapter;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messageList = new ArrayList<>();
+
+        recyclerView=findViewById(R.id.recycler_view);
+        welcomeTextView=findViewById(R.id.welcome_text);
+        messageEditText=findViewById(R.id.message_edit_text);
+        sendButton=findViewById(R.id.send_btn);
+
+        //setup recycler view
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm=new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
+
+        sendButton.setOnClickListener(v -> {
+           String question = messageEditText.getText().toString().trim();
+           addTOChat(question, Message.SEND_BY_ME);
+           messageEditText.setText("");
+           welcomeTextView.setVisibility(View.GONE);
+        });
+    }
+
+    void addTOChat(String message, String sentBy){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message, sentBy));
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageList.size()-1);
+            }
+        });
     }
 }
